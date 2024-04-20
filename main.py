d584304d7d175eae4d8742c800c4d42bce6a5049
j3rin <jerrymic2255@gmail.com>
2024-04-20 17:39:32 +0530

    sample

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+
+@app.get('/sim_test')
+def sim_test(id:int|None = None):
+    if id:
+        return {id}
+    else:
+        return {"Hello world"}
